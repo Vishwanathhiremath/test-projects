e7dae8e3c17015b77d9803a318944f8637c6d022
Vishwanath Hiremath <vishwanathhiremath24@gmail.com>
2026-01-02 23:20:39 +0530

    Initial project setup

---
+++ test-spring-git/src/main/java/com/vishwa/git_test/TestSpringGitApplication.java
@@ -0,0 +1,13 @@
+package com.vishwa.git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringGitApplication.class, args);
+	}
+
+}
---
+++ test-spring-git/src/test/java/com/vishwa/git_test/TestSpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwa.git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
