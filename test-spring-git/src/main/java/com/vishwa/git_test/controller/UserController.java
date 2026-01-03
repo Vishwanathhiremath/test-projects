b8871740207d60f2db99e09bc91e2fa1d08ee1a5
Vishwanath Hiremath <vishwanathhiremath24@gmail.com>
2026-01-03 21:00:27 +0530

    Added UserController service by Dev1

--- test-spring-git/src/main/java/com/vishwa/git_test/controller/UserController.java
+++ test-spring-git/src/main/java/com/vishwa/git_test/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.vishwa.git_test.controller;
 
+import com.vishwa.git_test.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,9 @@ public class UserController {
 
     @GetMapping
     public String getUsers() {
-        return "All users";
+
+        UserService user = new UserService();
+        return "user found ( name : " + user.getUserName() + " , id : " + user.getUserId() + ") ";
+
     }
 }
